e3e0c8da1bec589281b817fb7050d5bf9cf92243
Altusha4 <240782@astanait.edu.kz>
2025-10-12 15:15:25 +0600

    User calls 3 high-level scenarios. Facade coordinates multiple devices

--- src/HomeAutomationFacade.java
+++ src/HomeAutomationFacade.java
@@ -7,4 +7,23 @@ public class HomeAutomationFacade {
         this.thermostat = thermostat;
         this.camera = camera;
     }
+    public void startPartyMode() {
+        System.out.println("\n--- Party Mode ---");
+        light.operate("Turn ON with dim effect");
+        music.operate("Play music loudly");
+    }
+    public void activateNightMode() {
+        System.out.println("\n--- Night Mode ---");
+        light.operate("Turn OFF");
+        thermostat.operate("Set to ECO mode");
+        camera.operate("Enable Security");
+        music.operate("Stop music");
+    }
+    public void leaveHome () {
+        System.out.println("\n--- Leave Home Mode ---");
+        light.operate("Turn OFF");
+        music.operate("Stop music");
+        thermostat.operate("Eco temperature");
+        camera.operate("Security ON");
+    }
 }

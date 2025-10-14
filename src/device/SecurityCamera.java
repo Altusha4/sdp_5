4ae6dd0a2ce2648300d2ac0ab381ba2cf46b9b15
Altusha4 <240782@astanait.edu.kz>
2025-10-14 22:02:33 +0600

    Simple camera: user can turn recording or motion detection on/off and simulate motion events

--- src/device/SecurityCamera.java
+++ src/device/SecurityCamera.java
@@ -1,8 +1,45 @@
 package device;
+import java.time.LocalTime;
+import java.util.Scanner;
 
 public class SecurityCamera implements Device {
+    boolean recording = false;
+    boolean motionDetect = false;
+
+    @Override
+    public String name() {
+        return "SecurityCamera";
+    }
+
     @Override
     public void operate(String command) {
-        System.out.println("Security Camera: " + command);
+        Scanner sc = new Scanner(System.in);
+
+        if (command.equals("record:on")) {
+            recording = true;
+            System.out.println("Camera recording is ON");
+        }
+        else if (command.equals("record:off")) {
+            recording = false;
+            System.out.println("Camera recording is OFF");
+        }
+        else if (command.equals("detect:on")) {
+            motionDetect = true;
+            System.out.println("Motion detecting is ON");
+        }
+        else if (command.equals("detect:off")) {
+            motionDetect = false;
+            System.out.println("Motion detecting is OFF");
+        }
+        else if (command.equals("simulate")) {
+            if(motionDetect) {
+                System.out.println("Motion detecting at" + LocalTime.now() + (recording ? " -> Recording..." : " -> Stanby"));
+            } else {
+                System.out.println("Motion delection is OFF. No action");
+            }
+        }
+        else {
+            System.out.println("Unknown command for Camera");
+        }
     }
 }

e672e8aa2c7fa7d02d1dbed8328a1cbd557a27b5
Altusha4 <240782@astanait.edu.kz>
2025-10-16 19:04:23 +0600

    Refactor SmartDeviceFactory: add null check and align decorator structure

--- src/factory/SmartDeviceFactory.java
+++ src/factory/SmartDeviceFactory.java
@@ -1,16 +1,29 @@
 package factory;
-
 import device.*;
 import decorator.*;
 
 public class SmartDeviceFactory {
     public static Device create(String type) {
+        if (type == null) {
+            System.out.println("Error: device type is null.");
+            return null;
+        }
         switch (type.toLowerCase()) {
-            case "light"      -> { return new VoiceControlDecorator(new EnergySavingDecorator(new Light())); }
-            case "thermostat" -> { return new Thermostat(); }
-            case "camera"     -> { return new RemoteAccessDecorator(new SecurityCamera()); }
-            case "music"      -> { return new VoiceControlDecorator(new MusicSystem()); }
-            default -> { System.out.println("Unknown device type: " + type); return null; }
+            case "light":
+                return new VoiceControlDecorator(
+                        new EnergySavingDecorator(
+                                new Light()));
+            case "thermostat":
+                return new Thermostat();
+            case "camera":
+                return new RemoteAccessDecorator(
+                        new SecurityCamera());
+            case "music":
+                return new VoiceControlDecorator(
+                        new MusicSystem());
+            default:
+                System.out.println("Unknown device type: " + type);
+                return null;
         }
     }
 }

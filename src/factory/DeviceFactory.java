c75f2d71e17ef449d0b84f83b2e237e77cf7ca22
Altusha4 <240782@astanait.edu.kz>
2025-10-16 19:02:34 +0600

    Refactor DeviceFactory: add null check and improve readability

--- src/factory/DeviceFactory.java
+++ src/factory/DeviceFactory.java
@@ -3,13 +3,19 @@ import device.*;
 
 public class DeviceFactory {
     public static Device createDevice(String type) {
-        type = type.toLowerCase();
-
-        switch (type) {
-            case "light": return new Light();
-            case "thermostat": return new Thermostat();
-            case "camera": return new SecurityCamera();
-            case "music": return new MusicSystem();
+        if (type == null) {
+            System.out.println("Error: device type is null.");
+            return null;
+        }
+        switch (type.toLowerCase()) {
+            case "light":
+                return new Light();
+            case "thermostat":
+                return new Thermostat();
+            case "camera":
+                return new SecurityCamera();
+            case "music":
+                return new MusicSystem();
             default:
                 System.out.println("Unknown device type: " + type);
                 return null;
